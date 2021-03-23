4e060c2d8f5024b3e8a9ac694f40ee7ee7018d97
a7md0 <a7m3d699@gmail.com>
2021-03-23 23:11:55 +0300

    Jumping mechanic for the using a raycast

--- Assets/Scripts/CharacterControl.cs
+++ Assets/Scripts/CharacterControl.cs
@@ -9,10 +9,18 @@ public class CharacterControl : MonoBehaviour {
     private Vector3 movement;
     private Rigidbody body;
 
+    private RaycastHit ray;
+    private bool hit = false;
+    private CapsuleCollider capsuleCollider;
+    private float height;
+
     // Use this for initialization
     void Start() {
         body = GetComponent<Rigidbody>();
         speed = jogSpeed;
+
+        capsuleCollider = GetComponent<CapsuleCollider>();
+        height = capsuleCollider.height;
     }
 
     // Update is called once per frame
@@ -36,7 +44,29 @@ public class CharacterControl : MonoBehaviour {
             transform.rotation = Quaternion.LookRotation(new Vector3(movement.x, 0, movement.z));
         }
 
+        float y = body.velocity.y;
+        if (Input.GetKeyDown(KeyCode.Space) && IsOnGround()) {
+            y = jumpPower;
+        }
+
         //Finally, change velocity to move body
-        body.velocity = new Vector3(movement.x, body.velocity.y, movement.z);
+        body.velocity = new Vector3(movement.x, y, movement.z);
+    }
+
+    private bool IsOnGround() {
+        Vector3 origin = transform.position + transform.up * (height - 0.1f);
+        Vector3 destination = -transform.up;
+
+        hit = Physics.Raycast(origin, destination, out ray, height);
+
+        //Optionally, we can draw the ray in the scene view (not in-game)  to visualize it for testing
+        Debug.DrawRay(origin, destination * height, Color.blue);
+
+        //We only process the result of the casted ray IF it hits anything.
+        if (hit && ray.collider.gameObject.tag == "Ground") {
+            return true;
+        }
+
+        return false;
     }
 }
