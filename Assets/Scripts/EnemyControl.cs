2c38f2cb9ff82eb749f081db3071a2722b79c1c8
a7md0 <a7m3d699@gmail.com>
2021-03-23 22:34:57 +0300

    180 field of view for the Dragon

--- Assets/Scripts/EnemyControl.cs
+++ Assets/Scripts/EnemyControl.cs
@@ -6,6 +6,8 @@ public class EnemyControl : MonoBehaviour {
     RaycastHit ray;
     bool hit;
 
+    public Transform targetPosition;
+
     // Use this for initialization
     void Start() {
         hit = false;
@@ -13,22 +15,24 @@ public class EnemyControl : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        Vector3 origin = transform.position + transform.up * 3;
-        Vector3 destination = transform.forward;
-        float distance = 5;
+        if (this.targetPosition != null) {
+            Vector3 origin = transform.position + transform.up * 3;
+            Vector3 destination = (this.targetPosition.position - origin).normalized;
+            float distance = 7;
 
-        //We cast a ray and output the result to the ray object.
-        //Because this method returns a boolean, we assign it to the "hit" boolean
-        //so that we check if our casted ray has hit anything.
-        hit = Physics.Raycast(origin, destination, out ray, distance);
+            //We cast a ray and output the result to the ray object.
+            //Because this method returns a boolean, we assign it to the "hit" boolean
+            //so that we check if our casted ray has hit anything.
+            hit = Physics.Raycast(origin, destination, out ray, distance);
 
-        //Optionally, we can draw the ray in the scene view (not in-game)  to visualize it for testing
-        Debug.DrawRay(origin, destination * distance, Color.red, 0.01f);
+            //Optionally, we can draw the ray in the scene view (not in-game)  to visualize it for testing
+            Debug.DrawRay(origin, destination * distance, Color.red);
 
-        //We only process the result of the casted ray IF it hits anything.
-        if (hit) {
-            if (ray.collider.gameObject.tag == "Player") {
-                print("GOTCHA!");
+            //We only process the result of the casted ray IF it hits anything.
+            if (hit) {
+                if (ray.collider.gameObject.tag == "Player") {
+                    print("GOTCHA!");
+                }
             }
         }
     }
---
+++ Assets/Scripts/FieldTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldTrigger : MonoBehaviour {
+    private EnemyControl enemyControl;
+
+    private void Awake() {
+        this.enemyControl = this.GetComponentInParent<EnemyControl>();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            this.enemyControl.targetPosition = other.gameObject.transform;
+        }
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            this.enemyControl.targetPosition = other.gameObject.transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            this.enemyControl.targetPosition = null;
+        }
+    }
+}
